4bda9924e749df7db9732938a855d78bc03693ca
sudarshan-uprety <mr.sudarshanuprety@gmail.com>
2024-08-19 09:56:09 +0545

    order confirmation mail

--- apps/email_events/send_mail.py
+++ apps/email_events/send_mail.py
@@ -42,3 +42,26 @@ def forget_password_mail(to, otp, name):
         receivers=[to],
         html=content
     )
+
+
+def order_confirmation_mail(to, order_details):
+    mail = connect_mail()
+    template = templates.get_template('order_email.html')
+
+    context = {
+        'order_number': order_details['order_id'],
+        'items': order_details['items'],
+        'total': order_details['total'],
+        'payment_id': order_details['payment_id'],
+        'payment_amount': order_details['payment_amount'],
+        'payment_type': order_details['payment_type'],
+        'payment_service': order_details['payment_service'],
+    }
+    content = template.render(context)
+
+    mail.send(
+        subject=f"Order Confirmation #{order_details['order_id']}",
+        sender=EMAIL_SENDER,
+        receivers=[to],
+        html=content
+    )
